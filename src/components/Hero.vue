539d555800da76e33da1568effb1c6162a83d31f
Amnay <amnay.m@gmail.com>
2018-08-05 15:59:36 +0200

    fix nav

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="hero is-warning is-large is-bold">
+  <section class="hero is-warning is-fullheight is-large is-bold">
       <div class="hero-head">
           <nav class="navbar">
               <div class="container">
@@ -8,7 +8,7 @@
                           Octopus Railways
                       </a>
                   </div>
-                  <div id="navbarMenu" class="navbar-menu" :class="{'is-active': !burgerActive}">
+                  <div id="navbarMenu" class="navbar-menu" :class="{'is-active': navActive}">
                       <div class="navbar-end">
                           <div class="tabs is-center">
                               <ul>
@@ -24,18 +24,18 @@
                           </div>
                       </div>
                   </div>
-                <a role="button" class="navbar-burger" aria-label="navbarMenu" aria-expanded="false" :class="{'is-active': burgerActive}" @click="toggleMenu">
-                  <span aria-hidden="true"></span>
-                  <span aria-hidden="true"></span>
-                  <span aria-hidden="true"></span>
+                <a role="button" class="navbar-burger" data-target="navbarMenu" :class="{'is-active': navActive}" @click="toggleMenu">
+                  <span></span>
+                  <span></span>
+                  <span></span>
                 </a>
               </div>
           </nav>
       </div>
       <div class="hero-body">
-        <div class="columns">
-          <app-search-form class="column is-5 is-offset-1"></app-search-form>
-          <div class="column is-2 is-offset-2">
+        <div class="container columns">
+          <app-search-form class="column is-6 is-offset-1"></app-search-form>
+          <div class="column is-3 is-offset-3">
             <img src="../assets/octopus.svg" id="logo" alt="Logo">
           </div>          
         </div>
@@ -51,12 +51,12 @@ export default {
   },
   data() {
     return {
-      burgerActive: true
+      navActive: false
     }
   },
   methods: {
     toggleMenu() {
-      this.burgerActive = !this.burgerActive
+      this.navActive = !this.navActive
     }
   }
   
@@ -65,11 +65,10 @@ export default {
 
 
 <style lang="scss" scoped>
+
 #logo {
   max-width: 100%;
   height: auto;
 }
-
-
 
 </style>
